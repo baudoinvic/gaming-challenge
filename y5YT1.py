c9c50fcd39b6b184f07672178ff4653d25a46fdd
baudoin <baudoinvicbolingo@gmail.com>
2024-02-19 14:42:28 +0200

    today commit

--- y5YT1.py
+++ y5YT1.py
@@ -1,23 +1,23 @@
 
-import random
+# import random
 
-print('Rock-Paper-Scissors Game')
+# print('Rock-Paper-Scissors Game')
 
-choices = ['rock', 'scissors', 'paper']
+# choices = ['rock', 'scissors', 'paper']
 
-my_hand = 2
+# my_hand = 2
 
-computer_hand = random.randint(0, 2)
+# computer_hand = random.randint(0, 2)
 
-print('Your choice:', choices[my_hand])
-print("Computer's choice:", choices[computer_hand])
+# print('Your choice:', choices[my_hand])
+# print("Computer's choice:", choices[computer_hand])
 
-# Determine the winner
-if my_hand == computer_hand:
-    print("It's a tie!")
-elif (my_hand == 0 and computer_hand == 1) or \
-     (my_hand == 1 and computer_hand == 2) or \
-     (my_hand == 2 and computer_hand == 0):
-    print("You win!")
-else:
-    print("Computer wins!")
+# # Determine the winner
+# if my_hand == computer_hand:
+#     print("It's a tie!")
+# elif (my_hand == 0 and computer_hand == 1) or \
+#      (my_hand == 1 and computer_hand == 2) or \
+#      (my_hand == 2 and computer_hand == 0):
+#     print("You win!")
+# else:
+#     print("Computer wins!")
